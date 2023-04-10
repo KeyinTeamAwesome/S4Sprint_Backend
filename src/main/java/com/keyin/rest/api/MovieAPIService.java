7076e02a9f9a8dd634f297fc42f35abf6c82a3ba
DeToxFox <davidbrockturner@hotmail.com>
2023-04-10 21:25:48 -0230

    Pushing initial data to mySQL

--- src/main/java/com/keyin/rest/api/MovieAPIService.java
+++ src/main/java/com/keyin/rest/api/MovieAPIService.java
@@ -4,11 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "movie", path = "movie")
+// DT ADDED April 10, 2023 - @Transactional
+@Transactional
 public interface MovieAPIService extends JpaRepository<Movie, Long> {
+
+    // DT added April 10, 2023 public List<Movie> findAll();
+    public List<Movie> findAll();
 
     public List<Movie> findByTitle(@Param("title") String title);
     public List<Movie> findByGenre(@Param("genre")String genre);

778fa45dd3445b51a8ea9df2c109a05ad4f07984
glen may <ellisonglenmay@gmail.com>
2023-04-11 11:19:27 -0230

    cors fix

--- src/main/java/com/keyin/rest/api/MovieAPIService.java
+++ src/main/java/com/keyin/rest/api/MovieAPIService.java
@@ -3,12 +3,15 @@ package com.keyin.rest.api;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "movie", path = "movie")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public interface MovieAPIService extends JpaRepository<Movie, Long> {
 
 

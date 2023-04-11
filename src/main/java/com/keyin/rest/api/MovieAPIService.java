daf30a942d06ef427ab2c44bc039cc9698cc89ab
MakenzieRoberts <makenzie.roberts@keyin.com>
2023-04-11 12:54:20 -0230

    Added find by all (partial match) endpoint

--- src/main/java/com/keyin/rest/api/MovieAPIService.java
+++ src/main/java/com/keyin/rest/api/MovieAPIService.java
@@ -1,6 +1,7 @@
 package com.keyin.rest.api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,16 +15,24 @@ import java.util.List;
 
 public interface MovieAPIService extends JpaRepository<Movie, Long> {
 
+    /* Partial Match (All Fields) */
+    @Query("SELECT m FROM Movie m WHERE " +
+            "LOWER(m.title) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(m.genre) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(m.year) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(m.review) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    public List<Movie> findByAllContaining(@Param("searchTerm") String searchTerm);
 
-    public List<Movie> findByTitle(@Param("title") String title);
-    public List<Movie> findByGenre(@Param("genre")String genre);
-    public List<Movie> findByYear(@Param("year")int year);
-    public List<Movie> findByReview(@Param("review")int review);
-
-
+    /* Partial Match */
     public List<Movie> findByTitleContaining(@Param("title")String title);
     public List<Movie> findByGenreContaining(@Param("genre")String genre);
     public List<Movie> findByYearContaining(@Param("year")int year);
     public List<Movie> findByReviewContaining(@Param("review")int review);
+
+    /* Full Match */
+    public List<Movie> findByTitle(@Param("title") String title);
+    public List<Movie> findByGenre(@Param("genre")String genre);
+    public List<Movie> findByYear(@Param("year")int year);
+    public List<Movie> findByReview(@Param("review")int review);
 
 }

8792cecb51eb53494c937c9290f7729ec9524b81
kbalsom <kara.balsom@keyin.com>
2023-04-12 16:19:32 -0230

    findBy tests

---
+++ src/test/java/com/keyin/rest/api/RestServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.keyin.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestServiceApplicationTest {
+    @Test
+    void contextLoads() { }
+
+
+}
+
+

053ac910715cf715b0207a328ea316c0074486dc
glen may <ellisonglenmay@gmail.com>
2023-04-19 12:36:27 -0230

    completed project

--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,10 +1,3 @@
---CREATE TABLE public.movie (
---  id INT4 NULL,
---  title VARCHAR(200) NULL,
---  genre VARCHAR(200) NULL,
---  review INT4 NULL,
---  CONSTRAINT movie_pkey PRIMARY KEY (id)
---)
 drop table if exists movie;
 create table movie (
 id integer not null auto_increment,
@@ -13,3 +6,4 @@ review integer not null,
 title varchar(255) not null,
 primary key (id)
 )
+
